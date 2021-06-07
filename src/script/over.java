a92ea04a48e518ddf2c4d522f64397f2245b5a8d
Gabaniu <franek.rv7@gmail.com>
2021-06-08 00:08:04 +0200

    b

---
+++ src/script/over.java
@@ -0,0 +1,42 @@
+package script;
+
+import javax.swing.*;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class over extends JPanel implements MouseListener {
+    public game game;
+
+
+    public over(game game) {
+        this.game = game;
+        setBounds(0, 0, 1280, 760);
+        addMouseListener(this);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
